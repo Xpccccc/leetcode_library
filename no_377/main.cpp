8eaad142dfdfe8a7c688215112394c54f9c1d064
Xpccccc <1014647664@qq.com>
2024-06-24 15:04:54 +0800

    879. 盈利计划

--- no_377/main.cpp
+++ no_377/main.cpp
@@ -10,9 +10,11 @@ public:
         vector<double> dp(target + 1);
         dp[0] = 1;
         for (int i = 1; i <= target; ++i) {
-            for (int j = 0; j < n; ++j)
-                if (i >= nums[j])
+            for (int j = 0; j < n; ++j){
+                if (i >= nums[j]) // 确定了最后一个元素为nums[j]，那么只需要看前面有多少种方法组成i - nums[j]，且一个nums[j]就是一种情况
                     dp[i] += dp[i - nums[j]];
+            }
+
         }
         return dp[target];
     }
