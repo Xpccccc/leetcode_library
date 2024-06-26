3fd0bc88df158c39477eafd81e30b9f05fd49ceb
Xpccccc <1014647664@qq.com>
2024-06-26 15:29:53 +0800

    191. 位1的个数

---
+++ no_191/cmake-build-debug/CMakeFiles/no_191.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/no_191.dir/main.c.o"
+  "CMakeFiles/no_191.dir/main.c.o.d"
+  "no_191"
+  "no_191.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/no_191.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
